e227bf704cc9470fddcb54802f3a69043738dbba
mzp <mzpppp@gmail.com>
2015-05-14 17:04:25 +0900

    more robust

--- lib/ruboty/handlers/health_check.rb
+++ lib/ruboty/handlers/health_check.rb
@@ -1,7 +1,7 @@
 module Ruboty
   module Handlers
     class HealthCheck < Base
-      on /health (?<url>\S+) "(?<ok>[^"]*)" "(?<ng>[^"]*)"/, description: 'health check website', name: :check
+      on /health (?<url>\S+) +"(?<ok>[^"]*)" +"(?<ng>[^"]*)"/, description: 'health check website', name: :check
 
       def check(message)
         Ruboty::HealthCheck::Actions::Check.new(message).call
